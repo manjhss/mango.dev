724b02c9b20245c83676de5edd7d0833f24dc33f
Surendra Manjhi <manjhisurendra08@gmail.com>
2026-02-22 17:22:38 +0530

    Chore: move Blogs Page to /blogs folder

--- apps/demo/app/[locale]/page.tsx
+++ apps/demo/app/[locale]/page.tsx
@@ -1,27 +1,3 @@
-"use client";
-
-import { BlogExample } from "@/components/blog-example";
-
-import { TranslatedPost } from "@/lib/data";
-import { useEffect, useState } from "react";
-
 export default function Page() {
-  const [posts, setPosts] = useState<TranslatedPost[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("/api/posts", { cache: "force-cache" })
-      .then((res) => res.json())
-      .then(({ data, status, message }) => {
-        if (status === "error") throw new Error(message);
-        setPosts(data);
-      })
-      .finally(() => setLoading(false));
-  }, []);
-
-  return (
-    <main>
-      <BlogExample posts={posts} loading={loading} />
-    </main>
-  );
+  return <div>get started here</div>;
 }
